7ab4870c2aab814da17f13824fb7802839b23a6b
Fernando Tirado <fernando.tb.95@gmail.com>
2019-03-10 23:42:03 +0100

    Añadido último ejercicio y retoques

--- pythonpokemon.py
+++ pythonpokemon.py
@@ -8,7 +8,7 @@ def listar(doc):
 def contartipos(doc):
     tipo=input("Indica el tipo del Pokemon: ")
     listatipo=doc.xpath("/pokedex/pokemon[type='%s']/name/text()"%(tipo))
-    print(len(listatipo))
+    print("Hay",len(listatipo),"Pokemon de tipo %s" %(tipo))
 
 def mostrartipo(doc):
     pokemon=input("Indica de qué Pokemon quiere saber el tipo: ").upper()
@@ -19,8 +19,16 @@ def mostrartipo(doc):
 def mostraratkpokemon(doc):
     ataque=input("Indica un ataque: ")
     print("Los Pokemon que pueden aprender este ataque son: ")
+    print("")
     listaataque=doc.xpath("/pokedex/pokemon/moves/move[name='%s']/../../name/text()"%(ataque))
     return listaataque
+
+def ejer5(doc):
+    tipo=input("Indica el tipo del Pokemon: ")
+    atk=int(input("Indica el ataque máximo: "))
+    print("")
+    listapkm=doc.xpath("/pokedex/pokemon[type='%s']/stats[ATK<'%d']/../name/text()"%(tipo,atk))
+    return listapkm
 
 
 print("")
@@ -34,17 +42,24 @@ while True:
     opcion=int(input("Elija opción: "))
     
     if opcion==1:
+        print("")
         for i in listar(doc):
-            print(i)
+            print("-",i)
+        print("")
     elif opcion==2:
         contartipos(doc)
     elif opcion==3:
         for i in mostrartipo(doc):
             print(i," ",end="")
         print("")
+        print("")
     elif opcion==4:
         for i in mostraratkpokemon(doc):
-            print(i," ",end="")
+            print("-",i)
+        print("")
+    elif opcion==5:
+        for i in ejer5(doc):
+            print("-",i)
         print("")
     elif opcion==0:
         print("Adios")
